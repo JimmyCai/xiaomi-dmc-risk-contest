53d956683b1d503cd4b9da6067225b527b9b0f43
limingcai <limingcai@limingcaideMacBook-Pro.local>
2017-10-03 15:20:18 +0800

    add blend ensemble

--- src/test/scala/Md5.scala
+++ src/test/scala/Md5.scala
@@ -1,6 +1,11 @@
+import com.xiaomi.ad.over_sample.Repetition
+
 /**
   * Created by limingcai on 2017/10/3.
   */
 object Md5 {
-
+    def main(args: Array[String]): Unit = {
+        val ans = Repetition.MD5("ddd")
+        println(ans)
+    }
 }
